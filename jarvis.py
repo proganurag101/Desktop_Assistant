34b78de5f414eb636fde9131ebc47a7a11bebc5b
proganurag101 <anurag94530@gmail.com>
2021-06-29 23:40:59 +0530

    'jarvis'

---
+++ jarvis.py
@@ -0,0 +1,33 @@
+import pyttsx3
+import datetime
+
+engine = pyttsx3.init('sapi5')
+voices = engine.getProperty('voices')
+
+
+engine.setProperty('voice',voices[0].id)
+
+def speak(audio):
+    engine.say(audio)
+    engine.runAndWait()
+
+def wishMe():
+    hour = int(datetime.datetime.now().hour)
+    if hour>=0 and hour<12:
+        speak("Good Morning!")
+
+    elif hour>=12 and hour<18:
+        speak("good afternoon")
+
+    else:
+        speak("Good Evening")
+
+    speak("I am jarvis sir.please tell me how can i help you?")
+
+
+
+if __name__ == "__main__":
+    speak("Anurag is a good boy")
+    wishMe()
+
+
